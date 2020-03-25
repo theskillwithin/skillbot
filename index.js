94e3dca73b7248a4cb3fdbdf3c34c4efbf460fec
theskillwithin <austinppeterson@gmail.com>
2020-03-25 16:53:07 -0600

    rickroll

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 "use strict";
 require("dotenv").config({ path: ".env" });
 const irc = require("irc");
+const fetch = require("node-fetch");
+const get = require("lodash/get");
 const client = new irc.Client("chat.freenode.net", "skillbot", {
   channels: ["#theskillwithin"]
 });
@@ -11,7 +13,7 @@ const register = () => {
 
   setTimeout(() => {
     client.join("#theskillwithin");
-    client.join("##javascript");
+    // client.join("##javascript");
   }, 10000);
 };
 
@@ -23,6 +25,7 @@ client.addListener("message#theskillwithin", function(from, message) {
       "#theskillwithin",
       `Warning! ${from}: You have used a greek question mark(u037E) instead of a semicolon(u003B)!`
     );
+    ``;
   }
 });
 
@@ -34,3 +37,31 @@ client.addListener("message##javascript", function(from, message) {
     );
   }
 });
+
+function getYoutubeId(message) {
+  const regexp = /((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)(?<id>([\w\-]+)(\S+)?)/g;
+  const exec = regexp.exec(message);
+  return exec && exec.groups && exec.groups.id;
+}
+
+const youtubeURL = id =>
+  `https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=${id}=${process.env.YOUTUBE}`;
+
+const rickRoll = async (from, message) => {
+  const id = getYoutubeId(message);
+  if (id) {
+    await fetch(youtubeURL(id)).then(res => {
+      const title = get(res, "items[0].snippet.title", false);
+      if (/Rick Astley/gi.test(title)) {
+        client.say(
+          "#theskillwithin",
+          `Warning! ${from}: That video may possible be a rick roll!`
+        );
+      }
+    });
+  }
+};
+
+client.addListener("message#theskillwithin", (from, message) =>
+  rickRoll(from, message)
+);
