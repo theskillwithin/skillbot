300b6e9903af31e156612a441abc2379cf2206d4
theskillwithin <austinppeterson@gmail.com>
2021-05-19 16:03:04 -0600

    add libera

--- index.js
+++ index.js
@@ -10,25 +10,32 @@ const client = new irc.Client("chat.freenode.net", "skillbot", {
   realName: "skillbot",
 });
 
-const register = () => {
-  client.say("NickServ", `IDENTIFY skillbot ${process.env.IDENTIFY}`);
+const clientLibera = new irc.Client("irc.libera.chat", "skillbot", {
+  channels: ["#theskillwithin"],
+  userName: "skillbot",
+  realName: "skillbot",
+});
+
+const register = (c) => {
+  c.say("NickServ", `IDENTIFY skillbot ${process.env.IDENTIFY}`);
 
   setTimeout(() => {
-    client.join("#theskillwithin");
-    client.join("##javascript");
-    client.join("#ketochat");
-    client.join("#gatsbyjs");
-    client.join("#nextjs");
+    c.join("#theskillwithin");
+    c.join("##javascript");
+    c.join("#ketochat");
+    c.join("#gatsbyjs");
+    c.join("#nextjs");
   }, 10000);
 };
 
 const ignoreList = ["skillbot", "jellobot", "ecmabot"];
 
-client.addListener("registered", register);
+client.addListener("registered", register(client));
+clientLibera.addListener("registered", register(clientLibera));
 
-const greekQuestionMark = (from, message, channel) => {
+const greekQuestionMark = (from, message, channel, c) => {
   if (/\u037E/g.test(message)) {
-    client.say(
+    c.say(
       channel,
       `Warning! ${from}: You have used a greek question mark(u037E) instead of a semicolon(u003B)!`
     );
@@ -51,7 +58,8 @@ const greekQuestionMark = (from, message, channel) => {
 // };
 
 const getYoutubeId = (message) => {
-  const regexp = /((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)(?<id>([\w\-]+)(\S+)?)/g;
+  const regexp =
+    /((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)(?<id>([\w\-]+)(\S+)?)/g;
   const exec = regexp.exec(message);
   return exec && exec.groups && exec.groups.id;
 };
@@ -59,7 +67,7 @@ const getYoutubeId = (message) => {
 const youtubeURL = (id) =>
   `https://www.googleapis.com/youtube/v3/videos?part=id%2C+snippet&id=${id}&key=${process.env.YOUTUBE}`;
 
-const youtubeTitle = async (from, message, channel) => {
+const youtubeTitle = async (from, message, channel, c) => {
   const id = getYoutubeId(message);
   if (id) {
     const splitTimeFromId = id.split("?t=");
@@ -78,7 +86,7 @@ const youtubeTitle = async (from, message, channel) => {
       .then((res) => {
         const title = get(res, "items[0].snippet.title", false);
         if (title) {
-          client.say(channel, `YouTube Title: ${title}`);
+          c.say(channel, `YouTube Title: ${title}`);
         }
         // if (/Rick Astley/gi.test(title)) {
         //   client.say(
@@ -106,7 +114,7 @@ const isModToQuarterUp = (number) => {
   return isModToQuarterUp;
 };
 
-const calcWeight = (from, message, channel) => {
+const calcWeight = (from, message, channel, c) => {
   if (message.charAt(0) === ">") {
     const kiloPoundsConversionNumber = 2.20462;
 
@@ -121,7 +129,7 @@ const calcWeight = (from, message, channel) => {
       const isPoundsModToQuaterUp = isModToQuarterUp(pounds);
 
       const truncPounds = Math.trunc(pounds);
-      return client.say(
+      return c.say(
         channel,
         `${kilos} kilos is about ${
           isPoundsModToQuaterUp ? truncPounds + 1 : truncPounds
@@ -140,7 +148,7 @@ const calcWeight = (from, message, channel) => {
       const isKilosModToQuaterUp = isModToQuarterUp(kilos);
 
       const truncKilos = Math.trunc(kilos);
-      return client.say(
+      return c.say(
         channel,
         `${pounds} pounds is about ${
           isKilosModToQuaterUp ? truncKilos + 1 : truncKilos
@@ -158,7 +166,7 @@ const calcWeight = (from, message, channel) => {
 
       const poundsExact = kilosExact * kiloPoundsConversionNumber;
       const roundPoundsExact = Math.round(poundsExact * 100) / 100;
-      return client.say(
+      return c.say(
         channel,
         `${kilosExact} kilos is about ${roundPoundsExact} pounds`
       );
@@ -173,7 +181,7 @@ const calcWeight = (from, message, channel) => {
 
       const kilosExact = poundsExact / kiloPoundsConversionNumber;
       const roundKilosExact = Math.round(kilosExact * 100) / 100;
-      return client.say(
+      return c.say(
         channel,
         `${poundsExact} pounds is about ${roundKilosExact} kilos`
       );
@@ -183,42 +191,68 @@ const calcWeight = (from, message, channel) => {
 
 client.addListener("message#theskillwithin", (from, message) => {
   if (!ignoreList.includes(from.toLowerCase())) {
-    greekQuestionMark(from, message, "#theskillwithin");
-    youtubeTitle(from, message, "#theskillwithin");
-    calcWeight(from, message, "#theskillwithin");
+    greekQuestionMark(from, message, "#theskillwithin", client);
+    youtubeTitle(from, message, "#theskillwithin", client);
+    calcWeight(from, message, "#theskillwithin", client);
   }
 });
 
 client.addListener("message##javascript", (from, message) => {
   if (!ignoreList.includes(from.toLowerCase())) {
-    greekQuestionMark(from, message, "##javascript");
-    youtubeTitle(from, message, "##javascript");
+    greekQuestionMark(from, message, "##javascript", client);
+    youtubeTitle(from, message, "##javascript", client);
+  }
+});
+
+clientLibera.addListener("message#theskillwithin", (from, message) => {
+  if (!ignoreList.includes(from.toLowerCase())) {
+    greekQuestionMark(from, message, "#theskillwithin", clientLibera);
+    youtubeTitle(from, message, "#theskillwithin", clientLibera);
+    calcWeight(from, message, "#theskillwithin", clientLibera);
+  }
+});
+
+clientLibera.addListener("message#ketochat", (from, message) => {
+  if (!ignoreList.includes(from.toLowerCase())) {
+    youtubeTitle(from, message, "#ketochat", clientLibera);
+    calcWeight(from, message, "#ketochat", clientLibera);
+  }
+});
+
+clientLibera.addListener("message##javascript", (from, message) => {
+  if (!ignoreList.includes(from.toLowerCase())) {
+    greekQuestionMark(from, message, "##javascript", clientLibera);
+    youtubeTitle(from, message, "##javascript", clientLibera);
   }
 });
 
 client.addListener("message#ketochat", (from, message) => {
   if (!ignoreList.includes(from.toLowerCase())) {
-    youtubeTitle(from, message, "#ketochat");
-    calcWeight(from, message, "#ketochat");
+    youtubeTitle(from, message, "#ketochat", client);
+    calcWeight(from, message, "#ketochat", client);
   }
 });
 
 client.addListener("message#gatsbyjs", (from, message) => {
   if (!ignoreList.includes(from.toLowerCase())) {
-    greekQuestionMark(from, message, "#gatsbyjs");
-    youtubeTitle(from, message, "#gatsbyjs");
+    greekQuestionMark(from, message, "#gatsbyjs", client);
+    youtubeTitle(from, message, "#gatsbyjs", client);
   }
 });
 
 client.addListener("message#nextjs", (from, message) => {
   if (!ignoreList.includes(from.toLowerCase())) {
-    greekQuestionMark(from, message, "#nextjs");
-    youtubeTitle(from, message, "#nextjs");
+    greekQuestionMark(from, message, "#nextjs", client);
+    youtubeTitle(from, message, "#nextjs", client);
   }
 });
 
 client.addListener("pm", function (from, message) {
   client.say("#theskillwithin", `${from}: ${message}`);
+});
+
+clientLibera.addListener("pm", function (from, message) {
+  clientLibera.say("#theskillwithin", `${from}: ${message}`);
 });
 
 client.addListener("error", (message) => {
